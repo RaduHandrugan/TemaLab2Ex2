874b19b85d17e2eade20e32f0bcdfb0904ff7db2
RaduHandrugan <radu.handrugan@student.upt.ro>
2025-01-10 12:52:17 +0200

    Laboratorul2Pb2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+2. Fișierul cantec_in.txt conține versurile unui cântec la alegere. Să se scrie un
+program care creează fișierul cantec_out.txt, care conține versurile cântecului original dar în
+plus în dreptul fiecărui rând sunt afișate numărul de cuvinte de pe rând şi numărul de vocale
+de pe fiecare rând. În dreptul rândurilor care se încheie cu o grupare de litere aleasă se va
+pune o steluță (*). Rândurile pentru care un număr generat aleator este mai mic decât 0.1 vor
+fi scrise cu majuscule (se vor genera aleator numere între 0 şi 1).
+Se va defini o clasă Vers, care are o variabilă membră privată un șir de caractere
+reprezentând versul și se va dezvolta câte un operator pentru fiecare cerință de mai sus (o
+metodă care returnează numărul de cuvinte, o metodă care returnează numărul de vocale, etc).
+Se va crea un vector de obiecte de tip Vers care va conține informația preluată din fișierul de
+intrare.
+ */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String inputFile = "E:\\Java\\Proiecte\\Tema Lab 2 - Ex2\\src\\cantec_in.txt";
+        String outputFile = "cantec_out.txt";
+
+        String grupareLitere = "la";    //am ales gruparea "la" . unde aceasta se afla la sf de rand va fi pusa *
+
+        Random random = new Random();
+
+        try   //citire fisier + eroare la citire
+        {
+            List<Vers> versuri = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(inputFile)))
+            {
+                String line;
+                while ((line = br.readLine()) != null)
+                {
+                    versuri.add(new Vers(line));
+                }
+            }
+
+            //scriere in fisier
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile)))
+            {
+                for (Vers vers : versuri)
+                {
+                    int numarCuvinte = vers.getNumarCuvinte();
+                    int numarVocale = vers.getNumarVocale();
+
+                    boolean areStea = vers.terminare(grupareLitere);   // verificare daca versul e terminat in gruparea mentionata
+
+                    if (random.nextDouble() < 0.1)    //transf. majuscule daca nr generat e mai mic 0.1
+                    {
+                        vers.transformaInMajuscule();
+                    }
+
+                    String outputLine = vers.getText() + " Nr. de cuvinte in vers: " + numarCuvinte + " Nr de vocale in vers: " + numarVocale;
+                    if (areStea)
+                    {
+                        outputLine += " *";
+                    }
+
+                    bw.write(outputLine);
+
+                    bw.newLine();
+                }
+            }
+
+            System.out.println("Fisier creat cu succes! " + outputFile);
+        } catch (IOException e)
+        {
+            System.out.println("Eroare la fisier!" + e.getMessage());
         }
     }
 }
--- src/Vers.java
+++ src/Vers.java
@@ -1,4 +1,69 @@
-package PACKAGE_NAME;
+class Vers
+{
+    private String text;
 
-public class Vers {
+    public Vers(String text)
+    {
+        this.text = text;
+    }
+    // numararea de cuv.
+    public int getNumarCuvinte()
+    {
+        if (text.trim().length() == 0)
+        {
+            return 0;
+        }
+        String[] cuvinte = text.split(" ");  //am folosit fct split pt a diviza un string in mai multe parti
+        int count = 0;
+        for (String cuvant : cuvinte)
+        {
+            if (cuvant.length() > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNumarVocale()          //Numararea vocalelor
+    {
+        int count = 0;
+        char[] litere = text.toLowerCase().toCharArray();
+        for (char c : litere)
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean terminare(String grupare)
+    {
+        if (text.length() < grupare.length())
+        {
+            return false;
+        }
+        String sfarsit = text.substring(text.length() - grupare.length());
+        return sfarsit.equals(grupare);
+    }
+
+    public void transformaInMajuscule()                       //transformarea in majuscule
+    {
+        char[] litere = text.toCharArray();
+        for (int i = 0; i < litere.length; i++)
+        {
+            if (litere[i] >= 'a' && litere[i] <= 'z')
+            {
+                litere[i] = (char) (litere[i] - 'a' + 'A');
+            }
+        }
+        text = new String(litere);
+    }
+
+   //get set
+    public String getText()
+    {
+        return text;
+    }
 }
